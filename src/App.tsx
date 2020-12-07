d3ac138bdaa3cfff662b2f781622bce94993ceaf
AerospaceXu <aerospacexu@gmail.com>
2020-12-07 21:21:33 +0800

    feat: create Nav component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-import {
-  HashRouter as Router, Switch, Route, Link,
-} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './views/Home';
 import ComponentButtonPage from './views/ComponentButtonPage';
-
-const Nav = styled.nav`
-  width: 100%;
-
-  > ul > li {
-    margin: 4px 12px;
-    font-size: 18px;
-    color: rgba(0, 0, 0, 0.72);
-
-    &:hover {
-      color: rgba(0, 0, 0, 0.87);
-    }
-  }
-`;
+import Nav from './Layouts/Nav';
 
 const App: React.FC = () => (
   <Router>
-    <Nav className="center">
-      <ul className="center">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/components/buttons">按钮</Link>
-        </li>
-      </ul>
-    </Nav>
+    <Nav />
     <Switch>
       <Route path="/" exact>
         <Home />
---
+++ src/Layouts/Nav.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  width: 100%;
+  height: 88px;
+  padding: 0 24px;
+  margin-bottom: 32px;
+
+  > ul {
+    height: 100%;
+    width: 100%;
+    padding-bottom: 12px;
+    max-width: 1200px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.54);
+    align-items: flex-end;
+
+    > li {
+      margin: 0 24px;
+      font-size: 20px;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.64);
+      transition: 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+
+      &:hover {
+        color: rgba(0, 0, 0, 0.87);
+      }
+
+      > a.nav-active {
+        color: rgba(0, 0, 0, 0.87);
+      }
+    }
+  }
+`;
+
+const Nav: React.FC = () => (
+  <Wrapper className="center">
+    <ul className="center">
+      <li>
+        <NavLink to="/" activeClassName="nav-active" exact>
+          首页
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/components/buttons" activeClassName="nav-active">
+          按钮
+        </NavLink>
+      </li>
+    </ul>
+  </Wrapper>
+);
+
+export default Nav;
